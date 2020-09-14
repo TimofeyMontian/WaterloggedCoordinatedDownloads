3e2da50bb00d37283b324bea6bed6493b59c68d8
TimofeyMontian <tmontian@yandex.ru>
2020-09-14 09:36:00 +0000

    5

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace 
-int a = 3;
-if (a % 2 == 0)  
-else 
-}
+cout ('введите число x'); 
+cin >>(x); 
+cout ('введите  число y'); 
+cin >>(y); 
+cout ('введите число z'); 
+cin>>(z);  
+if (x<=3) and (x>=1) <<cout ('x-принадлежит интервалу') else writeln ('x не принадлежит и интервалу')>>;
+if (y<=3) and (y>=1) << cout ('y- принадлежит интервалу') else cout ('y не принадлежит и интервалу')>>;
+if (z<=3) and (z>=1) << cout ('z-принадлежит интервалу') else cout  ('z не принадлежит и интервалу')>>; 
+readln;
