5c1a487c8b4c4d3232e9050c5f3d6037c51b6f50
TimofeyMontian <tmontian@yandex.ru>
2020-09-14 08:42:38 +0000

    задача 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+using namespace 
 
-int main() {
-  std::cout << "Hello World!\n";
+ int main() {
+  int a;
+  int b;
+  cout <<"Введите 2 числа"<<endl;
+  сin >>a;
+  cin >>b;
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  cout <<"Числа поменялись местами:" <<endl <<b <<endl <<a;
+  return 0;
 }
