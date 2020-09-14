6b0d1fecd8080af2b6933fc1ab683e50c5d37392
TimofeyMontian <tmontian@yandex.ru>
2020-09-14 09:02:41 +0000

    4

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
 using namespace 
-
- int main() {
-  int a;
-  int b;
-  cout <<"Введите 2 числа"<<endl;
-  сin >>a;
-  cin >>b;
-  a=a+b;
-  b=a-b;
-  a=a-b;
-  cout <<"Числа поменялись местами:" <<endl <<b <<endl <<a;
-  return 0;
+int a = 3;
+if (a % 2 == 0)  
+else 
 }
